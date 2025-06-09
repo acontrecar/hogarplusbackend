ec3e5c890c104c5021dfc2731c1055bf18269b34
Antonio Contreras <acontrecar@gmail.com>
2025-06-09 22:36:31 +0200

    feat: implement getAllByHome, payDebtMember, and deleteDebt methods in DebtsService; enhance DebtsController to handle responses with appropriate HTTP status

--- src/debts/debts.controller.ts
+++ src/debts/debts.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  HttpStatus,
+} from '@nestjs/common';
 import { DebtsService } from './debts.service';
 import { CreateDebtDto } from './dto/create-debt.dto';
 import { UpdateDebtDto } from './dto/update-debt.dto';
@@ -15,7 +26,32 @@ export class DebtsController {
   @Post()
   async create(@Body() createDebtDto: CreateDebtDto, @GetUser() user: User, @Req() req: Request) {
     const result = await this.debtsService.create(createDebtDto, user.id);
-    return buildResponse(result, req.url, 'Debt created successfully');
+    return buildResponse(result, req.url, 'Debt created successfully', HttpStatus.CREATED);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('home/:homeId')
+  async getAllByHome(@Param('homeId') homeId: string, @GetUser() user: User, @Req() req: Request) {
+    const result = await this.debtsService.getAllByHome(+homeId, user.id);
+    return buildResponse(result, req.url, 'Debts retrieved successfully', HttpStatus.OK);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('pay/:debtMemberId')
+  async payDebtMember(
+    @Param('debtMemberId') debtMemberId: string,
+    @GetUser() user: User,
+    @Req() req: Request,
+  ) {
+    const result = await this.debtsService.payDebtMember(+debtMemberId, user.id);
+    return buildResponse(result, req.url, 'Debt member paid successfully', HttpStatus.OK);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':debtId')
+  async deleteDebt(@Param('debtId') debtId: string, @GetUser() user: User, @Req() req: Request) {
+    const result = await this.debtsService.deleteDebt(+debtId, user.id);
+    return buildResponse(result, req.url, 'Debt deleted paid successfully', HttpStatus.OK);
   }
 
   @Get()
--- src/debts/debts.service.ts
+++ src/debts/debts.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateDebtDto } from './dto/create-debt.dto';
 import { UpdateDebtDto } from './dto/update-debt.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -100,6 +105,117 @@ export class DebtsService {
 
       return { message: 'Debts created successfully' };
     });
+  }
+
+  async getAllByHome(homeId: number, userId: number) {
+    const member = await this.memberHomeRepository.findOne({
+      where: { user: { id: userId }, home: { id: homeId } },
+      relations: ['home', 'user'],
+    });
+
+    if (!member) {
+      throw new NotFoundException(`Member with id ${userId} not found`);
+    }
+
+    const debts = await this.debtRepository.find({
+      where: { home: { id: homeId } },
+      relations: [
+        'home',
+        'creditor',
+        'creditor.user',
+        'affectedMembers',
+        'affectedMembers.debtor',
+        'affectedMembers.debtor.user',
+      ],
+      order: { createdAt: 'DESC' },
+    });
+
+    const response = debts.map((debt, index) => ({
+      id: debt.id,
+      index: index,
+      description: debt.description,
+      homeName: debt.home.name,
+      amount: debt.amount,
+      status: debt.status,
+      createdAt: debt.createdAt,
+      creditor: {
+        id: debt.creditor.user.id,
+        name: debt.creditor.user.name,
+        avatar: debt.creditor.user.avatar,
+      },
+      affectedMembers: debt.affectedMembers.map((am) => ({
+        id: am.debtor.user.id,
+        debtMemberId: am.id,
+        name: am.debtor.user.name,
+        amount: am.amount,
+        isPaid: am.isPaid,
+        avatar: am.debtor.user.avatar,
+      })),
+    }));
+
+    return { debts: response };
+  }
+
+  async payDebtMember(debtMemberId: number, userId: number) {
+    const debtMember = await this.debtMemberRepository.findOne({
+      where: { id: debtMemberId },
+      relations: ['debt', 'debtor', 'debtor.user'],
+    });
+
+    if (!debtMember) {
+      throw new NotFoundException(`Debt member with id ${debtMemberId} not found`);
+    }
+
+    if (debtMember.debtor.user.id !== userId) {
+      throw new ForbiddenException('You cannot pay this debt member');
+    }
+
+    if (debtMember.isPaid) {
+      throw new BadRequestException('This debt member has already been paid');
+    }
+
+    await this.dataSource.transaction(async (manager) => {
+      debtMember.isPaid = true;
+
+      await manager.save(debtMember);
+
+      const allDebtMembers = await manager.find(DebtMember, {
+        where: { debt: { id: debtMember.debt.id } },
+      });
+
+      const allPaid = allDebtMembers.every((member) => member.isPaid);
+
+      if (allPaid) {
+        await manager.update(Debt, debtMember.debt.id, {
+          status: DebtStatus.COMPLETED,
+        });
+      }
+    });
+
+    return { debtMember };
+  }
+
+  async deleteDebt(debtId: number, userId: number) {
+    const debt = await this.debtRepository.findOne({
+      where: { id: debtId },
+      relations: ['creditor', 'creditor.user', 'home', 'affectedMembers', 'affectedMembers.debtor'],
+    });
+
+    if (!debt) {
+      throw new NotFoundException(`Debt with id ${debtId} not found`);
+    }
+
+    // return { debt };
+
+    if (debt.creditor.user.id !== userId) {
+      throw new ForbiddenException('You cannot delete this debt');
+    }
+
+    await this.debtRepository.delete({
+      id: debtId,
+    });
+
+    return { debtIdDelete: debt.id };
   }
 
   findAll() {
